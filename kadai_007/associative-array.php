608ecd2ba019a40ffbaa4df3e7357d0da3e47282
ohagi39 <future19960923@gmail.com>
2024-12-15 17:29:01 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>kadai_007</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    // 連想配列を作成する
+    $Array = ['name'=>'onion','price'=>200,'weight'=>160];
+
+    // 値を出力する
+    print_r($Array);
+  ?>
+  </p>
+</body>
+</html>
+
+
