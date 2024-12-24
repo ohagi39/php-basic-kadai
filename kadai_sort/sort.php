8deda8f0a6f411625572665315ab930f66e15d28
ohagi39 <future19960923@gmail.com>
2024-12-24 21:48:35 +0900

    Add files via upload

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -13,23 +13,21 @@
       function sort_2way($array, $order) {
 
       if ($order === true) {
+          echo "昇順にソートします。<br>";
           sort($array);
         } else {
+          echo "降順にソートします。<br>";
           rsort($array);
         }
         foreach ($array as $num) {
-          echo $num . '<br>';
+           echo $num . '<br>';
         }
       }
 
       $nums = [15, 4, 18, 23, 10];
 
-      
-      
-      echo "昇順にソートします。<br>";
       sort_2way($nums, true);
 
-      echo "降順にソートします。<br>";
       sort_2way($nums, false);
       ?>
       </p>
